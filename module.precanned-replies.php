50521ddf299330975871b60b5fd433437c401499
acognet <anne-catherine.cognet@combodo.com>
2022-03-21 17:18:52 +0100

    Change version number to 1.2.2 (remove -dev)

--- module.precanned-replies.php
+++ module.precanned-replies.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'precanned-replies/1.2.2-dev',
+	'precanned-replies/1.2.2',
 	array(
 		// Identification
 		//
