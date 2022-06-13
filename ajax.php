8112f4a2ef28f57c197dbd2598516230759093a6
acognet <anne-catherine.cognet@combodo.com>
2022-06-13 14:24:58 +0200

    N°3950 - Deprecate old unreferenced methods that are @deprecated

--- ajax.php
+++ ajax.php
@@ -1,7 +1,11 @@
 <?php
 require_once('../../approot.inc.php');
 require_once(APPROOT.'/application/application.inc.php');
-if (version_compare(ITOP_DESIGN_LATEST_VERSION , '3.0') < 0) {
+//remove require itopdesignformat at the same time as version_compare(ITOP_DESIGN_LATEST_VERSION , '3.0') < 0
+if (! defined("ITOP_DESIGN_LATEST_VERSION")) {
+	require_once APPROOT.'setup/itopdesignformat.class.inc.php';
+}
+if (version_compare(ITOP_DESIGN_LATEST_VERSION, '3.0') < 0) {
 	require_once(APPROOT.'/application/ajaxwebpage.class.inc.php');
 }
 require_once(APPROOT.'/application/wizardhelper.class.inc.php');
